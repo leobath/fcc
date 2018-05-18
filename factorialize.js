e25df777b96cc9c731e8e4257ecb817c7f2c4ce6
leo bath <36091485+leobath@users.noreply.github.com>
2018-05-19 02:35:54 +0300

    Create factorialize.js

---
+++ factorialize.js
@@ -0,0 +1,8 @@
+var factorial = function(number) {
+    if (number <= 0) { // terminal case
+      return 1;
+    } else { // block to execute
+      return (number * factorial(number - 1));
+    }
+  };
+  console.log(factorial(6));
