88ef10fb6900910d19aa0dac5feba94b8217b795
leobath <leoneedass@gmail.com>
2018-05-19 16:18:16 +0300

    various fixes

--- myReplace.js
+++ myReplace.js
@@ -1,38 +1,27 @@
 
-function myReplace(str, before, after) {
-    
+function myReplace(str, before, after) {    
     console.log(before);
     console.log(after);
     var newstr2 = str.split(' ');
-
     var newstr3 = after.split('');
     for (i in newstr2){
         console.log(newstr2[i]);
-
         if(newstr2[i] === before){
             if((before.charCodeAt(0) >= 65 ) && (before.charCodeAt(0) <= 90 )){
                 var dummy = after[0].toUpperCase();
                 newstr3[0] = newstr3[0].toUpperCase(); 
                 newstr3 = newstr3.join('');
-                after[0] = after[0].toUpperCase();  //ATTENTION Doesn't work. Must be splited.             
-                
+                after[0] = after[0].toUpperCase();  //ATTENTION Doesn't work. Must be splited.           
                 console.log("Hi I'am the 2nd if: "+after);
-    
                 newstr2[i] = newstr3;
             } else {
                 newstr2[i] = after;
             }
-
-
-
         }
-    }
-    
+    }    
 
     var newstr = newstr2.join(' ');
-
-    console.log(newstr);
- 
+    console.log(newstr); 
     return newstr;
   }
   
