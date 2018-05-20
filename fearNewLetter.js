886eefac3dcb7e6e8839ef691ce3a44c183008a7
leobath <leoneedass@gmail.com>
2018-05-20 21:44:01 +0300

    ver1

--- fearNewLetter.js
+++ fearNewLetter.js
@@ -1,8 +1,23 @@
 function fearNotLetter(str) {
+var splitString = []; 
+var matchArray = [];
+var i = 0;
+while(i<str.length){
+  splitString[i] = str.charCodeAt(i);
+  i++;
+}
 
-    console.log(str);
-    return str;
-  }
+//console.log(splitString[0]);
+for(var j = splitString[0]; j<=splitString[splitString.length-1]; j++){
+  matchArray.push(j);
+}
+console.log(matchArray);
+
+console.log(splitString[splitString.length-1]);
+return str;
+}
+
+
   
-  fearNotLetter("abce");
+fearNotLetter("abce");
   
