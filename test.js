3301f623125e8805d08cc0856d598158fae5b33b
leobath <leoneedass@gmail.com>
2018-05-20 22:32:13 +0300

    no test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var dum = 'A';
 
-var regexp = /[dum-E]/gi;
+var regexp = /[A-E]/gi;
 var matches_array = str.match(regexp);
 
 console.log(matches_array);
