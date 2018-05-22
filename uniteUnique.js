cca9531bf7e39ffb4b398a58aa1fbd7809a3e49f
leobath <leoneedass@gmail.com>
2018-05-22 23:17:00 +0300

    1st

---
+++ uniteUnique.js
@@ -0,0 +1,20 @@
+
+function uniteUnique(arr) {
+    var numberOfArguments =  arr.length;
+    console.log(numberOfArguments);
+    var i=0;
+    var argument = [];
+    while(i<arr.length){                 
+        for (var j=0 in arr){
+            console.log(arr);
+        }
+        i++;
+        
+    }
+    console.log(argument);
+
+    return arr;
+  }
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  
