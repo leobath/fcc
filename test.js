c764ca36a4be1849efbe152000f1ab0c95b4b407
leobath <leoneedass@gmail.com>
2018-05-23 07:46:54 +0300

    not working

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var dum = 'A';
+  
+function uniteUnique(arr) {
+    var array1 = [];
+    for (var i in arr){
+       // array1.push(arguments[i]);
+    array1[i] = arguments[i];
+    
+    }
+    var flattened = array1.reduce(
+        function(accumulator, currentValue) {
+          return accumulator.concat(currentValue);
+        },
+        []
+      );
+    
+      console.log(flattened);
+    
 
-var regexp = /[A-E]/gi;
-var matches_array = str.match(regexp);
-
-console.log(matches_array);
+    return arr;
+  }
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
--- uniteUnique.js
+++ uniteUnique.js
@@ -1,20 +1,81 @@
 
 function uniteUnique(arr) {
-    var numberOfArguments =  arr.length;
-    console.log(numberOfArguments);
-    var i=0;
-    var argument = [];
-    while(i<arr.length){                 
-        for (var j=0 in arr){
-            console.log(arr);
-        }
-        i++;
-        
+    var array1 = [];
+    for (var i in arr){
+       // array1.push(arguments[i]);
+    array1[i] = arguments[i];
+    
     }
-    console.log(argument);
 
-    return arr;
+    var array2 = array1[0];
+    //console.log(array2);
+    var mainArray = [];
+    var secondArray = [];
+
+    var arrLen = array1.length;
+    console.log(arrLen);
+
+    for(var i =0; i< arrLen; i++){
+        mainArray = array1[i];
+
+        for(var j=0; j<3; j++){
+            
+            
+                secondArray.push(mainArray[j]);
+               // console.log(secondArray.length);
+            }
+        
+    
+
+    }
+    console.log(mainArray);
+    // var x=0;
+    // var count=0;
+
+    // while(x<array1.length){
+    //     mainArray = array1[x];
+    //    for(var z=0; z<secondArray.length; z++){
+    //     for(var y=0; y<mainArray.length; y++){
+    //             if(mainArray[y] === secondArray[z]){
+    //                 count++;
+    //                 console.log("run if "+ count +" times");
+    //             }
+    //     }
+    //    }
+
+    //     x++;
+    // }
+    var newArray = [];
+    newArray.push(secondArray[0]);
+    var lengthOfNewArray = -1;
+    var count=0;
+    for(var x in secondArray){
+        lengthOfNewArray++;
+        for(var y=0; y<lengthOfNewArray; y++){
+            if(secondArray[x] === newArray[y] ){
+                count++;
+                if(count>1){
+                    //console.log("found");
+                secondArray[x] = undefined;
+                break;
+                }
+                
+            }else {
+                newArray.push(secondArray[x]);
+            }
+        }
+
+    }
+    secondArray = secondArray.filter(function(n){
+        return n !== undefined;
+    });
+    //console.log(mainArray);
+    console.log(secondArray);
+    //console.log(array1[1]);
+    //console.log(newArray);
+
+    return secondArray;
   }
   
-  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  uniteUnique([1, 2, 3], [5, 2, 1]);
   
