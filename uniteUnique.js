ebed0073f5db0396a9810ca9e9284c610a233cda
leobath <leoneedass@gmail.com>
2018-05-24 04:57:35 +0300

    reduce works

--- uniteUnique.js
+++ uniteUnique.js
@@ -4,78 +4,34 @@ function uniteUnique(arr) {
     for (var i in arr){
        // array1.push(arguments[i]);
     array1[i] = arguments[i];
-    
-    }
-
-    var array2 = array1[0];
-    //console.log(array2);
-    var mainArray = [];
-    var secondArray = [];
-
-    var arrLen = array1.length;
-    console.log(arrLen);
-
-    for(var i =0; i< arrLen; i++){
-        mainArray = array1[i];
-
-        for(var j=0; j<3; j++){
-            
-            
-                secondArray.push(mainArray[j]);
-               // console.log(secondArray.length);
-            }
-        
-    
-
-    }
-    console.log(mainArray);
-    // var x=0;
-    // var count=0;
-
-    // while(x<array1.length){
-    //     mainArray = array1[x];
-    //    for(var z=0; z<secondArray.length; z++){
-    //     for(var y=0; y<mainArray.length; y++){
-    //             if(mainArray[y] === secondArray[z]){
-    //                 count++;
-    //                 console.log("run if "+ count +" times");
-    //             }
-    //     }
-    //    }
-
-    //     x++;
-    // }
-    var newArray = [];
-    newArray.push(secondArray[0]);
-    var lengthOfNewArray = -1;
-    var count=0;
-    for(var x in secondArray){
-        lengthOfNewArray++;
-        for(var y=0; y<lengthOfNewArray; y++){
-            if(secondArray[x] === newArray[y] ){
-                count++;
-                if(count>1){
-                    //console.log("found");
-                secondArray[x] = undefined;
-                break;
-                }
-                
-            }else {
-                newArray.push(secondArray[x]);
-            }
-        }
-
-    }
-    secondArray = secondArray.filter(function(n){
+    array1 = array1.filter(function(n){
         return n !== undefined;
     });
-    //console.log(mainArray);
-    console.log(secondArray);
-    //console.log(array1[1]);
-    //console.log(newArray);
+    
+    }
+    
+    
+    console.log(array1);
+    var flattened = array1.reduce(
+        function(accumulator, currentValue) {
+          return accumulator.concat(currentValue);
+        },
+        []
+      );
+    
+    console.log(flattened);
 
-    return secondArray;
+    ///write your code under this line.
+
+    
+
   }
   
-  uniteUnique([1, 2, 3], [5, 2, 1]);
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  
+  //uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);
+  
+  //uniteUnique([1, 2, 3], [5, 2, 1]);
+  
+  //uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
   
