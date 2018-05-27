b78c8ec92db68e85bcc59418b20f59e32885a05b
leobath <leoneedass@gmail.com>
2018-05-27 10:07:14 +0300

    1st

---
+++ convertHTML.js
@@ -0,0 +1,19 @@
+function convertHTML(str) {
+    // &colon;&rpar;
+    var regex = /(\W+)\S(W+)/;
+    
+
+    switch (str) {
+        case "&":
+            str = str.replace
+            break;
+    
+        default:
+            break;
+    }
+
+    return str;
+  }
+  
+  convertHTML("Dolce & Gabbana");
+  
