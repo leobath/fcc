00bfe4f64f792d336d4e88ffbd27d1fd24f48fac
leobath <leoneedass@gmail.com>
2018-06-04 15:35:21 +0300

    comment print logs (serve for debugging).

--- uniteUnique.js
+++ uniteUnique.js
@@ -2,8 +2,8 @@
 function uniteUnique(arr) {
     var array1 = Array.from(arguments);
     var arrLength = array1.length;
-    console.log(arrLength);
-    console.log(array1);       
+    //console.log(arrLength);
+    //console.log(array1);       
     
     var flattened = array1.reduce(
         function(accumulator, currentValue) {
@@ -20,11 +20,11 @@ function uniteUnique(arr) {
     return newflattened;
   }
   
-  //uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
   
   uniteUnique([1, 3, 2], [1, [5]], [2, [4]]);
   
-  //uniteUnique([1, 2, 3], [5, 2, 1]);
+  uniteUnique([1, 2, 3], [5, 2, 1]);
   
-  //uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8], [2, 3], [5, 8, 8]);
+  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8], [2, 3], [5, 8, 8]);
   
