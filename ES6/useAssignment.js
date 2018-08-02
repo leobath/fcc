37d8ca50a43902100b60948ef9f02b6095395291
leobath <leoneedass@gmail.com>
2018-08-02 07:30:18 +0300

    upd

---
+++ ES6/useAssignment.js
@@ -0,0 +1,15 @@
+function getLength(str) {
+    "use strict";
+  
+    // change code below this line
+    const length = str; // change this
+
+    const len = {length: 12};
+    console.log(len.length);
+    // change code above this line
+  
+    return len.length; // you must assign length to len in line
+  
+  }
+  
+  console.log(getLength('FreeCodeCamp'));
