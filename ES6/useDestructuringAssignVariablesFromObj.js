991342621b3f2ace05c969b3c626696bbe9dd34f
leobath <leoneedass@gmail.com>
2018-08-11 20:22:07 +0300

    final solution

---
+++ ES6/useDestructuringAssignVariablesFromObj.js
@@ -0,0 +1,16 @@
+function getLength(str) {
+    "use strict";
+  
+    // change code below this line
+
+    
+    
+
+     // change this
+    const {length: len} = str; 
+    
+    return len; // you must assign length to len in line
+  
+  }
+  
+  console.log(getLength('FreeCodeCamp'));
