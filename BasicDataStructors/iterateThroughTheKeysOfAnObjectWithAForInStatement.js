277953bd3b04bc77c6b5411515449811e4b40ef8
leobath <leoneedass@gmail.com>
2018-08-17 01:28:58 +0300

    final solution

---
+++ BasicDataStructors/iterateThroughTheKeysOfAnObjectWithAForInStatement.js
@@ -0,0 +1,35 @@
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function countOnline(obj) {
+    // change code below this line
+    // console.log(obj.Alan.online);
+    let counter = 0;
+    for(let user in obj){
+     console.log(obj[user].online);
+     if(obj[user].online) {
+         counter++;
+     }
+    }
+  
+    return counter;
+    // change code above this line
+  }
+  
+  console.log(countOnline(users));
